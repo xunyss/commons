f88f75a52be2a6c039a8b2c91e137802b16dd3db
xunyss <xunyss@gmail.com>
2016-11-15 13:39:41 +0900

    init

---
+++ openssl/src/test/java/org/xunyss/openssl/OpenSSLTest.java
@@ -0,0 +1,12 @@
+package org.xunyss.openssl;
+
+import org.junit.Test;
+
+public class OpenSSLTest {
+	
+	@Test
+	public void testSSL() throws Exception {
+		OpenSSL openssl = new OpenSSL(System.out);
+		openssl.exec(new String[] {"asn1parse", "-genstr", "UTF8:hello-World"});
+	}
+}
